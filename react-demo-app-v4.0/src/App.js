8d8a902f7fedb8393db31d1f85d23883580864d1
NatGallagher <natgallagher@Nats-Air.attlocal.net>
2025-03-26 18:26:14 -0400

    About.js added and imported

--- react-demo-app-v4.0/src/App.js
+++ react-demo-app-v4.0/src/App.js
@@ -1,10 +1,17 @@
+import {HashRouter, Routes, Route} from "react-router-dom"
 import Home from "./components/Home"
+import About from "./components/About"
 
 function App() {
 
   return (
     <>
-      <Home/>
+      <HashRouter>         
+      <Routes>      
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+      </Routes>
+    </HashRouter>   
     </>
   );
 }
