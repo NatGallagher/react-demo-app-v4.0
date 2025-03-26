eb14affe7dc9fa05b6bcb04e3150c6ad3899567d
NatGallagher <natgallagher@Nats-Air.attlocal.net>
2025-03-26 18:18:48 -0400

    Home.js created and imported

--- react-demo-app-v4.0/src/App.js
+++ react-demo-app-v4.0/src/App.js
@@ -1,28 +1,10 @@
-
+import Home from "./components/Home"
 
 function App() {
 
-  function useRefDemo1() {
-    let msgText = "useRefDemo1";
-    console.log(msgText);
-  }
-
-  function useStateDemo1() {
-    let msgText = "useStateDemo1";
-    console.log(msgText);
-  }
-
-
   return (
     <>
-      <h2>React Demo App v4.0</h2>
-      <a href="#" onClick={useRefDemo1}>useRefDemo1</a>
-      <p></p>
-      <a href="#" onClick={useStateDemo1}>useStateDemo1</a>
-      <p></p>
-      <div>
-        [Display Info]
-      </div>
+      <Home/>
     </>
   );
 }
---
+++ react-demo-app-v4.0/src/components/Home.js
@@ -0,0 +1,29 @@
+function Home() {
+
+    function useRefDemo1() {
+      let msgText = "useRefDemo1";
+      console.log(msgText);
+    }
+  
+    function useStateDemo1() {
+      let msgText = "useStateDemo1";
+      console.log(msgText);
+    }
+  
+  
+    return (
+      <>
+        <h2>React Demo App v4.0</h2>
+        <a href="#" onClick={useRefDemo1}>useRefDemo1</a>
+        <p></p>
+        <a href="#" onClick={useStateDemo1}>useStateDemo1</a>
+        <p></p>
+        <div>
+          [Display Info]
+        </div>
+      </>
+    );
+  }
+  
+  export default Home;
+  
