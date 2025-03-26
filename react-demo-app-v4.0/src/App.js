820d9542d1891fa367ff8deddf243eb6d8a106cd
NatGallagher <natgallagher@Nats-Air.attlocal.net>
2025-03-26 18:15:09 -0400

    functions added and linked to buttons

--- react-demo-app-v4.0/src/App.js
+++ react-demo-app-v4.0/src/App.js
@@ -2,13 +2,23 @@
 
 function App() {
 
+  function useRefDemo1() {
+    let msgText = "useRefDemo1";
+    console.log(msgText);
+  }
+
+  function useStateDemo1() {
+    let msgText = "useStateDemo1";
+    console.log(msgText);
+  }
+
 
   return (
     <>
       <h2>React Demo App v4.0</h2>
-      <a href="#">useRefDemo1</a>
+      <a href="#" onClick={useRefDemo1}>useRefDemo1</a>
       <p></p>
-      <a href="#">useStateDemo1</a>
+      <a href="#" onClick={useStateDemo1}>useStateDemo1</a>
       <p></p>
       <div>
         [Display Info]
