8980c88bf31116aa77794350f75ecdb9bfbd8411
NatGallagher <natgallagher@Nats-Air.attlocal.net>
2025-03-26 18:12:37 -0400

    app.js edited

--- react-demo-app-v4.0/src/App.js
+++ react-demo-app-v4.0/src/App.js
@@ -1,9 +1,18 @@
 
 
 function App() {
+
+
   return (
     <>
       <h2>React Demo App v4.0</h2>
+      <a href="#">useRefDemo1</a>
+      <p></p>
+      <a href="#">useStateDemo1</a>
+      <p></p>
+      <div>
+        [Display Info]
+      </div>
     </>
   );
 }
